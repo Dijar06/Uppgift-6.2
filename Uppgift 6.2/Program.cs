a3af467576cd35fc8308ca1a59354fc94bda7781
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-14 10:54:23 +0100

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Störst(100, 101));
+        }
+
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 > tal2)
+            {
+                return tal1;
+            }
+
+            else if (tal1 < tal2)
+            {
+                return tal2;
+            }
+
+            else
+            {
+                return tal1;
+            }
+        }
+    }
+}
